ac8a7234d5aedd3e1d68ff18643b35679478ae99
RikuHirose <rikuparkour9@gmail.com>
2020-06-12 15:49:13 +0900

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
     }
 }
